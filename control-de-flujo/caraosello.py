f44116c548eae847af919f81050ff6a4f2783a8b
Cristofoo <cristofer.fuentes.manibet@alumnos.sip.cl>
2025-03-31 11:38:34 -0300

    2

---
+++ control-de-flujo/caraosello.py
@@ -0,0 +1,12 @@
+#Cristofer Fuentes
+#me duele la rodilla
+#31-03-2025
+
+import random
+
+num = random.randint(0, 1)
+
+
+if num > 0.5 :
+    print ('cara')
+else: print ('sello')
---
+++ control-de-flujo/promedio.py
@@ -0,0 +1,11 @@
+#Cristofer Fuentes
+#me duele la rodilla
+#31-03-2025
+
+nota =  int(input(" ¿ que nota te sacaste ?"))
+
+if nota >= 40 :
+    print("pasaste")
+
+else:
+    print("no pasaste")
